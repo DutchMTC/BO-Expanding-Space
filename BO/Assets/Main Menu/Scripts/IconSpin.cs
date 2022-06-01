db30f7ec898aeebb8f42359d4cf76d617c33ac36
Miyaki Truong <32574@ma-web.nl>
2022-06-01 14:07:16 +0200

    New main menu + added fade

---
+++ BO/Assets/Main Menu/Scripts/IconSpin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class IconSpin : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationSpeed = 5f;
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotationSpeed));
+    }
+}
--- BO/Assets/Main Menu/Scripts/StartButton.cs
+++ BO/Assets/Main Menu/Scripts/StartButton.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class StartButton : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animator;
+
+    public void Fade()
+    {
+        animator.SetTrigger("Fade Out");
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
