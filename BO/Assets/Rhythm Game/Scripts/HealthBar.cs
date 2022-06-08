0fafd2cdede4ffabb69a4a9929f7adb4cec71fe3
Miyaki <90682254+DutchMTC@users.noreply.github.com>
2022-06-08 20:33:47 +0200

    Added Health Bar

---
+++ BO/Assets/Rhythm Game/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int misses = animator.GetInteger("Misses");
+
+        slider.value = 1 - misses;
+    }
+}
